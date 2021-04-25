40c4966aaac69c82a182edb4441d1db4140420a7
Len Woodward <gopher@Lens-iMac.local>
2021-04-25 11:12:44 -0700

    remove unnecessary methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,21 +2,11 @@
 
 namespace ProjektGopher\WorkflowGenerator\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use ProjektGopher\WorkflowGenerator\WorkflowGeneratorServiceProvider;
 
 class TestCase extends Orchestra
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Spatie\\WorkflowGenerator\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
@@ -26,16 +16,5 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-
-        /*
-        include_once __DIR__.'/../database/migrations/create_laravel_workflow_generator_table.php.stub';
-        (new \CreatePackageTable())->up();
-        */
     }
 }
