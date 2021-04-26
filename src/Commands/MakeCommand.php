e455b7fdf31a1a2864e6fa5dfac5588d843a07e2
Len Woodward <gopher@Lens-iMac.local>
2021-04-25 17:00:38 -0700

    Finish make command

--- src/Commands/MakeCommand.php
+++ src/Commands/MakeCommand.php
@@ -7,7 +7,7 @@ use Illuminate\Filesystem\Filesystem;
 
 class MakeCommand extends Command
 {
-    public $signature = 'workflow:make {name?}';
+    public $signature = 'workflow:make {name?} {--force}';
 
     public $description = 'Make workflow from stub';
 
@@ -22,13 +22,44 @@ class MakeCommand extends Command
 
     public function handle()
     {
-        if (! $this->files->exists('.github/workflows')) {
-            $this->files->ensureDirectoryExists('.github/workflows');
+        if(! $workflow = $this->argument('name'))
+        {
+            $this->warn('You must specify the name of the workflow you\'d like to generate.');
+            $this->warn('Use php artisan workflow:list to show a list of a available workflows.');
+            return;
+        }
 
+        if(! $this->files->exists(__DIR__ . "/../../stubs/{$workflow}.stub"))
+        {
+            $this->warn("We can't find a workflow named {$workflow}.");
+            $this->warn('Use php artisan workflow:list to show a list of a available workflows.');
+            return;
+        }
+
+        if (! $this->files->exists('.github/workflows'))
+        {
+            $this->files->ensureDirectoryExists('.github/workflows');
             $this->info('Created .github/workflows directory.');
         }
 
-        #$this->call('');
+        if($this->files->exists(".github/workflows/{$workflow}.yml"))
+        {
+            if($this->option('force'))
+            {
+                $this->comment("Overwriting .github/workflows/{$workflow}.yml");
+            }
+            else
+            {
+                $this->warn(".github/workflows/{$workflow}.yml already exists.");
+                $this->comment("Try using php artisan workflow:make {$workflow} --force");
+                return;
+            }
+        }
+
+        $contents = $this->files->get(__DIR__ . "/../../stubs/{$workflow}.stub");
+        $this->files->put(".github/workflows/{$workflow}.yml", $contents);
+        $this->info('Workflow saved!');
+
         $this->comment('Making workflows all effin day.');
     }
 }
--- tests/Feature/Commands/MakeCommandTest.php
+++ tests/Feature/Commands/MakeCommandTest.php
@@ -19,18 +19,33 @@ class MakeCommandTest extends TestCase
     {
         $this->files->shouldReceive('exists')
             ->once()
+            ->andReturnTrue();
+
+        $this->files->shouldReceive('exists')
+            ->once()
             ->with('.github/workflows')
             ->andReturnFalse();
+        
+        $this->files->shouldReceive('exists')
+            ->once()
+            ->with('.github/workflows/stub.yml')
+            ->andReturnFalse();
+
         $this->files->shouldReceive('ensureDirectoryExists')
             ->once()
             ->with('.github/workflows')
             ->andReturnTrue();
-        // $this->files->shouldReceive('put')
-        //     ->with('draft.yaml', 'stub');
 
-        // $this->files->shouldReceive('exists')->with('app');
+        $this->files->shouldReceive('get')
+            ->once()
+            ->andReturn('stub');
 
-        $this->artisan('workflow:make')
+        $this->files->shouldReceive('put')
+            ->once()
+            ->with('.github/workflows/stub.yml', 'stub')
+            ->andReturnTrue();
+
+        $this->artisan('workflow:make stub')
             ->expectsOutput('Created .github/workflows directory.')
             ->assertExitCode(0);
     }
@@ -40,13 +55,124 @@ class MakeCommandTest extends TestCase
     {
         $this->files->shouldReceive('exists')
             ->once()
+            ->andReturnTrue();
+
+        $this->files->shouldReceive('exists')
+            ->once()
             ->with('.github/workflows')
             ->andReturnTrue();
-        $this->files->shouldNotReceive('makeDirectory');
-        // $this->files->shouldReceive('exists')
-        //     ->with('app');
+        $this->files->shouldNotReceive('ensureDirectoryExists');
 
-        $this->artisan('workflow:make')
+        $this->files->shouldReceive('exists')
+            ->once()
+            ->with('.github/workflows/stub.yml')
+            ->andReturnTrue();
+
+        $this->artisan('workflow:make stub')
+            ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function a_named_workflow_must_exist()
+    {
+        $this->files->shouldReceive('exists')
+            ->once()
+            ->andReturnFalse();
+
+        $this->artisan('workflow:make blargh')
+            ->expectsOutput('We can\'t find a workflow named blargh.')
+            ->expectsOutput('Use php artisan workflow:list to show a list of a available workflows.')
+            ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function it_creates_a_workflow_from_stub_with_provided_name()
+    {
+        $this->files->shouldReceive('exists')
+            ->once()
+            ->andReturnTrue();
+
+        $this->files->shouldReceive('exists')
+            ->once()
+            ->with('.github/workflows')
+            ->andReturnTrue();
+        $this->files->shouldNotReceive('ensureDirectoryExists');
+
+        $this->files->shouldReceive('exists')
+            ->once()
+            ->with('.github/workflows/stub.yml')
+            ->andReturnFalse();
+
+        $this->files->shouldReceive('get')
+            ->once()
+            ->andReturn('stub');
+
+        $this->files->shouldReceive('put')
+            ->once()
+            ->with('.github/workflows/stub.yml', 'stub')
+            ->andReturnTrue();
+
+        $this->artisan('workflow:make stub')
+            ->expectsOutput('Workflow saved!')
+            ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function it_will_not_overwrite_an_existing_workflow()
+    {
+        $this->files->shouldReceive('exists')
+            ->once()
+            ->andReturnTrue();
+
+        $this->files->shouldReceive('exists')
+            ->once()
+            ->with('.github/workflows')
+            ->andReturnTrue();
+        $this->files->shouldNotReceive('ensureDirectoryExists');
+
+        $this->files->shouldReceive('exists')
+            ->once()
+            ->with('.github/workflows/stub.yml')
+            ->andReturnTrue();
+
+        $this->files->shouldNotReceive('put');
+
+        $this->artisan('workflow:make stub')
+            ->expectsOutput('.github/workflows/stub.yml already exists.')
+            ->expectsOutput('Try using php artisan workflow:make stub --force')
+            ->assertExitCode(0);
+    }
+
+    /** @test */
+    public function it_will_overwrite_an_existing_workflow_if_the_force_option_is_present()
+    {
+        $this->files->shouldReceive('exists')
+            ->once()
+            ->andReturnTrue();
+
+        $this->files->shouldReceive('exists')
+            ->once()
+            ->with('.github/workflows')
+            ->andReturnTrue();
+        $this->files->shouldNotReceive('ensureDirectoryExists');
+
+        $this->files->shouldReceive('exists')
+            ->once()
+            ->with('.github/workflows/stub.yml')
+            ->andReturnTrue();
+
+        $this->files->shouldReceive('get')
+            ->once()
+            ->andReturn('stub');
+
+        $this->files->shouldReceive('put')
+            ->once()
+            ->with('.github/workflows/stub.yml', 'stub')
+            ->andReturnTrue();
+
+        $this->artisan('workflow:make stub --force')
+            ->expectsOutput('Overwriting .github/workflows/stub.yml')
+            ->expectsOutput('Workflow saved!')
             ->assertExitCode(0);
     }
 }
